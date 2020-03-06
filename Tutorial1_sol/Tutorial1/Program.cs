3b506f21a642fb0302e7b7ed6abe784cb380be23
s19047 <s19047@pjwstk.edu.pl>
2020-03-06 15:31:02 +0100

    vffgg

--- Tutorial1_sol/Tutorial1/Program.cs
+++ Tutorial1_sol/Tutorial1/Program.cs
@@ -10,25 +10,47 @@ namespace Tutorial1
         
       public  static async Task Main(string[] args)
         {
-            var websiteUrl = args[0];
+           
+            var websiteUrl = args.Length > 0 ? args[0] : throw new ArgumentNullException();
             var httpClient = new HttpClient();
-
-            var response = await httpClient.GetAsync(websiteUrl);
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var htmlContent = await response.Content.ReadAsStringAsync();
-
-                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                var emails = regex.Matches(htmlContent);
-
-                foreach (var email in emails)
+                var response = await httpClient.GetAsync(websiteUrl);
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine(email.ToString());
-                }
+                    var htmlContent = await response.Content.ReadAsStringAsync();
 
-                Console.WriteLine("This is dev branch");
+                    var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var emails = regex.Matches(htmlContent);
+
+                    if (emails.Count > 0) { 
+
+                    foreach (var email in emails)
+                    {
+                        Console.WriteLine(email.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no email addresses found");
+                }
+                    
+                }
             }
+            catch (Exception)
+            {
+
+                Console.WriteLine("error while dopwnloading page");
+            }
+            
+            if(websiteUrl == null)
+            {
+                throw new ArgumentNullException("The url should contain the value");
+            }
+         
+
+          
         }
     }
 }
