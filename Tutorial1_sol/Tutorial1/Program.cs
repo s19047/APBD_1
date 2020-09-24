d8cd32bb349dde5cee9ca917bff8fb4f3d5761b2
s19047 <61874844+s19047@users.noreply.github.com>
2020-09-25 00:22:45 +0200

    Update Program.cs

--- Tutorial1_sol/Tutorial1/Program.cs
+++ Tutorial1_sol/Tutorial1/Program.cs
@@ -1,34 +1,58 @@
-﻿using System;
+using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Tutorial1
 {
-    public class Program
-    {
-        
-      public  static async Task Main(string[] args)
-        {
-            var websiteUrl = args[0];
-            var httpClient = new HttpClient();
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			if (args.Length == 1)
+			{
+				if (args[0] != null)
+				{
+					var url = args[0];
+					var httpClient = new HttpClient();
+					var response = await httpClient.GetAsync(url);
+					if (response.IsSuccessStatusCode)
+					{
+						var htmlContent = await response.Content.ReadAsStringAsync();
 
-            var response = await httpClient.GetAsync(websiteUrl);
-            if (response.IsSuccessStatusCode)
-            {
-                var htmlContent = await response.Content.ReadAsStringAsync();
+						Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+						var emails = emailRegex.Matches(htmlContent);
+						if (emails != null)
+						{
+							var uniqueMatches = emails
+									.OfType<Match>()
+									.Select(m => m.Value)
+									.Distinct();
+							uniqueMatches.ToList().ForEach(Console.WriteLine);
+						}
+						else
+						{
+							Console.WriteLine("No email addresses found");
+						}
+					}
+					else
+					{
+						Console.WriteLine("An Error has occured");
+					}
+					httpClient.Dispose();
+					response.Dispose();
+				}
+				else
+				{
+					throw new ArgumentException("Please Pass a valid URL");
+				}
 
-                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                var emails = regex.Matches(htmlContent);
-
-                foreach (var email in emails)
-                {
-                    Console.WriteLine(email.ToString());
-                }
-
-                Console.WriteLine("hello");
-            }
-        }
-    }
+			}
+			else
+			{
+				throw new ArgumentNullException("Please Pass one Url argument");
+			}
+		}
+	}
 }
